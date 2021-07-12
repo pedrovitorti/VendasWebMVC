5efa2e734146a448e3b5e6108259277a9d25751f
Pedro Vitor <pedrovitorti@gmail.com>
2021-07-12 12:25:57 -0300

    MySQL adaptando e primeira migration

--- VendasWebMVC/Startup.cs
+++ VendasWebMVC/Startup.cs
@@ -36,8 +36,10 @@ namespace VendasWebMVC
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            services.AddDbContext<VendasWebMVCContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("VendasWebMVCContext")));
+            //services.AddDbContext<VendasWebMVCContext>(options =>
+            //        options.UseMySql(Configuration.GetConnectionString("VendasWebMVCContext") ));
+
+            services.AddDbContext<VendasWebMVCContext>(options => options.UseMySql(Configuration.GetConnectionString("VendasWebMVCContext"), builder => builder.MigrationsAssembly("VendasWebMVC")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
