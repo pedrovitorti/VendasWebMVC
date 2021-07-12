4dcb64906ef063405bf164677c813797f352c241
Pedro Vitor <pedrovitorti@gmail.com>
2021-07-12 10:20:29 -0300

    Primeiro Model-Controller-View Departamento

---
+++ VendasWebMVC/Models/Departamento.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VendasWebMVC.Models
+{
+    public class Departamento
+    {
+        public int Id { get; set; }
+        public String Nome { get; set; }
+    }
+}
